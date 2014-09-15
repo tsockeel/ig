5c8bac70181612c5b7fff6c1c63fd52efad2942d
tsockeel <teddy.sockeel@gmail.com>
2014-09-15 12:44:54 +0200

    change url config

--- ig/urls.py
+++ ig/urls.py
@@ -5,8 +5,8 @@ from ig import views
 urlpatterns = patterns('ig.views',
 	url(r'^$', views.home, name='home'),
 	url(r'^oauth/$', views.oauth, name='oauth'),
-	url(r'^tag/(?P<tagname>\w{0,50})/$$', views.tag, name='tag'),
+	url(r'^tag/(?P<tagname>\w{0,50})/$', views.tag, name='tag'),
 	url(r'^subtag/$', views.subtag, name='subtag'),
 	url(r'^rmsubtag/(?P<id>[0-9]+)$', views.rmsubtag, name='rmsubtag'),
-	url(r'^postupdate/$', views.postupdate, name='postupdate'),
+	url(r'^postupdate/', views.postupdate, name='postupdate'),
 )
