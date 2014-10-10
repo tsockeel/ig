e0af562d01f65f80ce8d2afd1c389a31c7ea18cd
tsockeel <teddy.sockeel@gmail.com>
2014-10-10 18:17:08 -0500

    clean wsgi

--- realtime/wsgi.py
+++ realtime/wsgi.py
@@ -13,10 +13,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realtime.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-
-
-# add from tuto
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
