5b1d5aa42d65b958672ecf405e438c550d4b73e1
Teddy Sockeel <teddy.sockeel@gmail.com>
2014-12-18 12:18:36 -0500

    add Pause/Resume feature - some variable for formatting

--- instaq/views.py
+++ instaq/views.py
@@ -6,27 +6,34 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from instaq.forms import EventForm , TagForm
-from ig.models import Event, Tag
+from ig.models import Event, Tag, Post
 from django.utils import timezone 
 from ig.views import createTagSubscription, deleteTagSubscription
 
 def home(request):
     user = request.user
     if user.is_authenticated():
+	nowDate = timezone.now()
         event_form = EventForm()
-        event_db = Event.objects.filter(owner=request.user)
+        event_db = Event.objects.filter(owner=request.user).order_by('-created_time')
         event_list = []
         for ev in event_db:
-            e = {}
-            e["event"] = ev
-            e["tagform"] = TagForm(initial={'event_id': ev.pk})
-            e["tags"] = Tag.objects.filter(event = ev)
-            event_list.append(e)
+        	e = {}
+	        e["event"] = ev
+        	e["tagform"] = TagForm(initial={'event_id': ev.pk})
+        	e["tags"] = Tag.objects.filter(event = ev)
+		e["posts"] = Post.objects.filter(event = ev)
+	    	e["activated"] =  ev.start_time < nowDate and ev.stop_time > nowDate
+            	event_list.append(e)
 
         return render_to_response('index.html', locals(), RequestContext(request))
     else:
         form = AuthenticationForm(request)
         return render_to_response('login.html', locals(), RequestContext(request))
+
+
+def livegallery(request, eventname):
+	return render_to_response('livegallery.html', locals(), RequestContext(request))
 
 
 def login_user(request):
@@ -73,6 +80,28 @@ def remove_event(request, eventid):
             	eventtoremove.delete()
         except Exception, e:
                 print 'remove_event exception: %s' % e
+        return redirect(home)
+
+
+@login_required
+def pause_event(request, eventid):
+        try:
+                currentEvent = Event.objects.get(pk = eventid)
+                currentEvent.paused = True
+		currentEvent.save()
+        except Exception, e:
+                print 'pause_event exception: %s' % e
+        return redirect(home)
+
+
+@login_required
+def resume_event(request, eventid):
+        try:    
+                currentEvent = Event.objects.get(pk = eventid)
+                currentEvent.paused = False
+                currentEvent.save()
+        except Exception, e:
+                print 'resume_event exception: %s' % e
         return redirect(home)
 
 
