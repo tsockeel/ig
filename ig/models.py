370618db2ed75c02147e2f524b5ce2b469eb2907
Teddy Sockeel <teddy.sockeel@gmail.com>
2014-12-18 12:16:38 -0500

    change format - clean code

--- ig/models.py
+++ ig/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
-
+from django.utils.timezone import localtime
 
 class Event(models.Model):
 	name = models.CharField(max_length=50)
@@ -10,6 +10,7 @@ class Event(models.Model):
 	created_time = models.DateTimeField(auto_now_add=True)
 	start_time = models.DateTimeField()
 	stop_time = models.DateTimeField()
+	paused = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.name
@@ -24,16 +25,11 @@ class Tag(models.Model):
 
 
 class Post(models.Model):
-#	username = models.CharField(max_length=100)
 	tag = models.ForeignKey(Tag)
-#	caption_text = models.CharField(max_length=2200)
 	instagram_id = models.CharField(max_length=50)
 	created_time = models.DateTimeField(auto_now_add=True)
 	url = models.URLField()
-#	media_type = models.CharField(max_length=20)
-#	media_url_thumbnail = models.URLField()
-#	media_url_stdres = models.URLField()
 	event = models.ForeignKey(Event)
 
 	def __unicode__(self):
-		return ((u'%s ' % self.instagram_id) + self.created_time.strftime("%Y-%m-%d %H:%M:%S --- ") + (u' id=%s' % self.instagram_id))
+		return ((u' id=%s - at ' % self.instagram_id) + localtime(self.created_time).strftime("%Y-%m-%d %H:%M:%S"))
